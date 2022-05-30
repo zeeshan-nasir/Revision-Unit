c323a21713513a92cf648da95b2fe30d97e16e03
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-05-30 10:01:07 +0530

    api rate limiting

---
+++ api-rate-limiting/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const needle = require("needle");
+const url = require("url");
+const rateLimit = require("express-rate-limit").default;
+const apicache = require("apicache");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_KEY = process.env.API_BASE_KEY;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+let cache = apicache.middleware;
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10,
+});
+
+app.use(limiter);
+app.set("trust proxy", 1);
+
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/getweather", cache("1 minutes"), async (req, res) => {
+    try {
+        const params = new URLSearchParams({
+            [API_BASE_KEY]: API_KEY_VALUE,
+            ...url.parse(req.url, true).query,
+        });
+
+        const apiRes = await needle(
+            "get",
+            `${API_BASE_URL}?${params}&units=metric`
+        );
+        const data = apiRes.body;
+        console.log(data);
+        if (process.env.NODE_ENV !== "production") {
+            console.log(`REQUEST1: ${API_BASE_URL}?${params}&units=metric`);
+        }
+
+        res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port : ", PORT);
+});
