0256cb54675df124e92595e18348c0e97122a357
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-06-01 16:53:29 +0530

    map with multiple markers added

--- map-it/index.js
+++ map-it/index.js
@@ -1,15 +1,33 @@
-function initMap() {
-    const uluru = { lat: -25.344, lng: 131.031 };
+var locations = [
+    ["Delhi", 28.6341, 77.2025, 4],
+    ["Gurugram", 28.4595, 77.0266, 5],
+    ["Ghaziabad", 28.6692, 77.4538, 3],
+];
 
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
-    });
+var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: new google.maps.LatLng(28.6041, 77.3025),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+});
 
-    const marker = new google.maps.Marker({
-        position: uluru,
+var infowindow = new google.maps.InfoWindow();
+
+var marker, i;
+
+for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map,
     });
-}
 
-window.initMap = initMap;
+    google.maps.event.addListener(
+        marker,
+        "click",
+        (function (marker, i) {
+            return function () {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            };
+        })(marker, i)
+    );
+}
