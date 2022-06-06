9f5ec7b44bba3c43ddb943a81c6cb44bdf3c6f11
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-06-06 17:29:24 +0530

    delete

--- food-app-fullStack/server/src/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-    return mongoose.connect(
-        "mongodb+srv://zeeshannasir:zeeshan@cluster0.iltt7.mongodb.net/eat"
-    );
-};
